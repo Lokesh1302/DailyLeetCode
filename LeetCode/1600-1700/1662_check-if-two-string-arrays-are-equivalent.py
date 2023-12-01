8aa09700ee8300a97603d72c9a86595dd16b758e
Lokesh Dogga <lokeshdogga1302@gmail.com>
2023-12-02 00:16:28 +0530

    Update 1662_check-if-two-string-arrays-are-equivalent.py

--- LeetCode/1600-1700/1662_check-if-two-string-arrays-are-equivalent.py
+++ LeetCode/1600-1700/1662_check-if-two-string-arrays-are-equivalent.py
@@ -2,3 +2,16 @@ class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
         return "".join(word1) == "".join(word2)
         
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        for c1, c2 in zip(self.generate(word1), self.generate(word2)):
+            if c1 != c2:
+                return False
+        return True
+
+    def generate(self, wordlist: List[str]):
+        for word in wordlist:
+            for char in word:
+                yield char
+        yield None

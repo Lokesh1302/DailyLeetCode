ee6f84037f56cad29023df13d5f44f61e057f608
Lokesh Dogga <lokeshdogga1302@gmail.com>
2024-03-04 23:31:11 -0500

    Update 1750-minimum-length-of-string-after-deleting-similar-ends.py

--- LeetCode/1700-1800/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ LeetCode/1700-1800/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -1,19 +1,37 @@
+# class Solution:
+#     def minimumLength(self, s: str) -> int:
+#         left, right = 0, len(s) - 1
+#         size = len(s) - 1
+
+#         while left < right:
+#             if s[left] == s[right]:
+#                 cur = s[left]
+#                 while left <= size and s[left] == cur:
+#                     left += 1
+                
+#                 while right >= 0 and s[right] == cur:
+#                     right -= 1
+#             else:
+#                 break
+#             if left > right:
+#                 return 0
+#         return right - left + 1
+             
+
 class Solution:
     def minimumLength(self, s: str) -> int:
         left, right = 0, len(s) - 1
         size = len(s) - 1
 
-        while left < right:
-            if s[left] == s[right]:
+        while left < right and s[left] == s[right]:
                 cur = s[left]
-                while left <= size and s[left] == cur:
+                
+                while left <= right and s[left] == cur:
                     left += 1
                 
-                while right >= 0 and s[right] == cur:
+                while right > left and s[right] == cur:
                     right -= 1
-            else:
-                break
-            if left > right:
-                return 0
+
         return right - left + 1
-             
+
+# Cleaner way of doing things.

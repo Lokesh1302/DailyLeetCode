19a983ae7252591eab03c51fded686da8bdec635
Lokesh Dogga <lokeshdogga1302@gmail.com>
2024-03-06 23:37:26 -0500

    Create 876-middle-of-the-linked-list.py

---
+++ LeetCode/800-900/876-middle-of-the-linked-list.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+        slow, fast = head, head.next
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        
+        if fast is not None and fast.next is None:
+            return slow.next
+        return slow

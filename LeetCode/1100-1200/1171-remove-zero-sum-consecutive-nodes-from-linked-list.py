c60d8821bc0f3987e22b07134a242d0cce3b5667
Lokesh Dogga <lokeshdogga1302@gmail.com>
2024-03-12 14:41:50 -0400

    Create 1171-remove-zero-sum-consecutive-nodes-from-linked-list.py

---
+++ LeetCode/1100-1200/1171-remove-zero-sum-consecutive-nodes-from-linked-list.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        front = ListNode(0, head)
+        current = front
+        prefix_sum = 0
+        prefix_sum_to_node = {0: front}
+
+        # Calculate the prefix sum for each node and add to the hashmap
+        # Duplicate prefix sum values will be replaced
+        while current is not None:
+            prefix_sum += current.val
+            prefix_sum_to_node[prefix_sum] = current
+            current = current.next
+
+        # Reset prefix sum and current
+        prefix_sum = 0
+        current = front
+
+        # Delete zero sum consecutive sequences 
+        # by setting node before sequence to node after
+        while current is not None:
+            prefix_sum += current.val
+            current.next = prefix_sum_to_node[prefix_sum].next
+            current = current.next
+
+        return front.next
